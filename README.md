0c4c660c9ce6b45f32492a11015a228ea5ec7fe6
raziel214 <soulreavers214@gmail.com>
2020-04-14 21:05:48 -0500

    in this commit, we are examine the types of variables

--- README.md
+++
@@ -1,2 +0,0 @@
-# CursoProfesionalJavascript
- Curso Profesional de JavaScript
--- ejercicios/TypeScript/index.ts
+++
@@ -1,7 +0,0 @@
-console.log('Hello, TypeScript');
-
-function add(a: number, b: number) {
-    console.log(a + b);
-}
-
-const sum = add(2, 3);
